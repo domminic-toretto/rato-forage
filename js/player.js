a7bc7f0129bc1046847ddca0c482f07c4aa87733
joao vitor <joaovsng@gmail.com>
2026-01-19 16:49:51 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -21,7 +21,7 @@ class Player {
         this.isAttacking = false;
         this.currentFrame = 0;
         this.frameTimer = 0;
-        this.frameDelay = 300; // ms entre frames (ajuste para mais suave/rápido)
+        this.frameDelay = 80; // ms entre frames (ajuste para mais suave/rápido)
         
         // Configuração das Sprite Sheets
         // Os tamanhos serão calculados automaticamente quando a imagem carregar
@@ -39,7 +39,7 @@ class Player {
                 loaded: false,
                 image: null,
                 frames: 5,
-                frameWidth: 200,    // Será calculado
+                frameWidth: 120,    // Será calculado
                 frameHeight: 64
                 ,   // Será calculado
                 loop: true,
