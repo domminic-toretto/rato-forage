7927027a15887be88081baa6dec6b767b91bd28b
joao vitor <joaovsng@gmail.com>
2026-01-18 22:06:38 -0300

    Fix frame dimensions for idle animation

--- js/player.js
+++ js/player.js
@@ -30,8 +30,8 @@ class Player {
                 loaded: false,
                 image: null,
                 frames: 1,
-                frameWidth: 64,   // rat-idle: 640x640
-                frameHeight: 64,
+                frameWidth: 640,   // rat-idle: 640x640
+                frameHeight: 640,
                 loop: true
             },
             walk: {
