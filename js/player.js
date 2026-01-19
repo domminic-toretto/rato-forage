c671c4b6253a5702da5f64f56f7cfd78ed680b3f
joao vitor <joaovsng@gmail.com>
2026-01-19 07:45:24 -0300

    Update frameHeight calculation in player.js

--- js/player.js
+++ js/player.js
@@ -39,7 +39,7 @@ class Player {
                 image: null,
                 frames: 5,         // rat-walk: 2176x3264 = 5 frames verticais
                 frameWidth: 3264,  // Largura total
-                frameHeight: 2176 /5,  // Altura dividida por 5 frames = 652.8px por frame
+                frameHeight: 10000 /5,  // Altura dividida por 5 frames = 652.8px por frame
                 loop: true,
                 vertical: true     // Frames empilhados verticalmente
             },
